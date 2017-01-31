95d2d33680a4ebb717d6f936fc4e325bdb8479b8
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-01-31 12:21:53 -0600

    more exercises

---
+++ list/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var limit = 3;
+            int[] source = { 0, 1, 1, 3, 2, 3, 4, 5 };
+            var query = from item in source where item <= limit select item;
+            foreach(int i in query)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ structs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs
+{
+    class Program
+    {
+        public struct Coordis
+        {
+            public int x, y;
+
+            public Coordis(int p1, int p2)
+            {
+                x = p1;
+                y = p2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Coordis coords = new Coordis(2, 5);
+            Console.Write("x = {0}, y = {1}", coords.x, coords.y);
+
+            Console.ReadLine();
+        }
+    }
+}
