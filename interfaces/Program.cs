8c4ad79f4ea2feac375453d16e4c42f0f9f763de
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-01-31 17:54:32 -0600

    interface example

---
+++ interfaces/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces
+{
+    interface IOperations
+    {
+        decimal sum();
+        decimal rest();
+        decimal multi();
+        decimal divide();
+    }
+
+    class Operations : IOperations
+    {
+        decimal x, y;
+
+        public Operations(decimal x, decimal y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public decimal sum()
+        {
+            return x + y;
+        }
+
+        public decimal rest()
+        {
+            return x - y;
+        }
+
+        public decimal multi()
+        {
+            return x * y;
+        }
+
+        public decimal divide()
+        {
+            return x / y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Which operation do yo want to do? (s, r, m, d): ");
+            string op = Console.ReadLine();
+
+            Console.Write("Set number one: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Set number two: ");
+            decimal y = decimal.Parse(Console.ReadLine());
+
+            decimal res = 0;
+
+            Operations ops = new Operations(x, y);
+
+            switch (op)
+            {
+                case "s":
+                    res = ops.sum();
+                    break;
+                case "r":
+                    res = ops.rest();
+                    break;
+                case "m":
+                    res = ops.multi();
+                    break;
+                case "d":
+                    res = ops.divide();
+                    break;
+            }
+
+            Console.Write("The result is: {0}", res);
+
+            Console.ReadLine();
+
+        }
+    }
+}
