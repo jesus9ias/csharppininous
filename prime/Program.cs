847d0e56a10690552158f204a8bbc1aaf7ff2567
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-01-31 09:55:17 -0600

    is prime project

---
+++ prime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkForNum = true;
+
+            while(checkForNum == true)
+            {
+                Console.Write("Write a number:\n");
+                Int64 num = Int64.Parse(Console.ReadLine());
+                int x = 2;
+                double sqr = Math.Sqrt(num);
+                bool isPrime = true;
+
+                while (x <= sqr && isPrime == true)
+                {
+                    if (num % x == 0)
+                    {
+                        isPrime = false;
+                    }
+                    else
+                    {
+                        x++;
+                    }
+                }
+
+                if (isPrime == true)
+                {
+                    Console.Write("The number {0} is prime and it was solved in {1} steps\n", num, x);
+                }
+                else
+                {
+                    Console.Write("The number {0} is not prime and it was solved in {1} steps\n", num, x);
+                }
+
+                Console.Write("Do you want to check another number?, y for 'Yes', any else for 'No':\n");
+                checkForNum = (Console.ReadLine() == "y");
+            }
+        }
+    }
+}
