a52d3788e1617c12e8984fd805a087d755289fe7
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-03-22 15:07:04 -0600

    almost done using sessions

---
+++ session/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using session.Models;
+
+namespace session.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Boolean checkLogin()
+        {
+            if (HttpContext.Session.Contents["isLogued"] == null || HttpContext.Session.Contents["isLogued"].ToString() == "false")
+            {
+                HttpContext.Session.Add("isLogued", false);
+                return false;
+            }
+            return true;
+        }
+        public ActionResult Index()
+        {
+            this.checkLogin();
+            return View();
+        }
+
+        public ActionResult Login()
+        {   
+            /*if (this.checkLogin() == true)
+            {
+                Response.Redirect("/Home");
+            }*/
+            return View();
+        }
+
+        public void Logout()
+        {
+            HttpContext.Session.Add("isLogued", false);
+            Response.Redirect("/Home/Login");
+        }
+
+        [HttpPost]
+        public void DoLogin(Credentials credentials)
+        {
+            if (credentials.User == "test" && credentials.Password == "test")
+            {
+                HttpContext.Session.Add("isLogued", true);
+                Response.Redirect("/Home");
+            }
+            Response.Redirect("/Home/Login");
+        }
+    }
+}
---
+++ session/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(session.Startup))]
+namespace session
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
